b690589be5f5443c5f800afaf877ea764c5fc611
Klaus Götz <klaus.klagoe.goetz@fau.de>
2019-11-11 10:52:04 +0100

    seperate scripts for NR and fit

---
+++ SASFIT/plot.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+def load_data(fn):
+  xs = []
+  ys = []
+  with open(fn, 'r', encoding='latin-1') as f:
+    for ctr,line in enumerate(f):
+      line = line.strip()
+      data = list(line.split())
+      if ctr == 0:
+        fields = int(len(data)/4)
+        for i in range(fields):
+          xs.append([])
+          ys.append([])
+        print(xs)
+      for i in range(fields):
+        print(4*i)
+        xs[i].append(float(data[4*i]))
+        ys[i].append(float(data[(4*i)+1]))
+
+  return xs, ys
+
+if __name__ == '__main__':
+  fn = sys.argv[1]
+  mode = sys.argv[2]
+  data = load_data(fn)
+  rc('font', **{'family': 'serif', 'serif':['Helvetica']})
+  rc('text', usetex = True)
+  fig, axes = plt.subplots()
+  
+  if mode == 'fit':
+    axes.loglog(data[0][1], data[1][1], 'bo', label = 'Small Particles')
+    axes.loglog(data[0][0], data[1][0], label = 'fit', lw = 2, c = 'black')
+    axes.set_xlabel(r'Q [$\AA^{-1}$]')
+    axes.set_ylabel(r'Int [a.u.]')
+  if mode == 'nr':
+    axes.plot(data[0][0], np.multiply(np.array(data[1][0]), np.power(np.array(data[0][0]),3)))
+    
+  ofn = fn.split('.')[0]+'.png'
+  print(ofn)
+  #plt.savefig(ofn)
+  plt.show()
