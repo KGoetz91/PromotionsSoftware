12c9a645e0531c4accd0aeaaca720a6d9dfd0ea2
Klaus Götz <kgoetz91@t-online.de>
2020-08-13 01:20:44 +0200

    Circular Mask started

--- SAXSSpace_Reducer/calibrate.py
+++ SAXSSpace_Reducer/calibrate.py
@@ -96,12 +96,33 @@ class DataSet:
     #print(mask[x-2:x+2,y-2:y+2])
     mask[mask >= 0] = 0
     mask[mask < 0] = 1
-    mask[300,409] = 1
-    mask[398,388] = 1
-    #mask[666,388] = 1
-    #mask[764,409] = 1
-    #mask[1006,274] = 1
-    mask[:int(self._com[0])]=1
+    mask2 = np.array(mask)
+    
+    radius = 30
+    alpha = 45
+    coordinates = []
+    for x in range(-radius, radius, 1):
+      for y in range(-radius, radius, 1):
+        if x*x+y*y<=radius*radius:
+          if (x>0) or (abs(y) > abs(x*np.tan(alpha*np.pi/180))):
+            coordinates.append((x,y))
+        
+    coordinates = [(x+int(self._com[0])+1,y+int(self._com[1])) for (x,y) in coordinates]
+    for (x,y) in coordinates:
+      mask2[x,y] = 1000
+      
+    #plt.imshow(mask2)
+    plt.imshow(np.log10(np.multiply(self._twoddata,np.multiply(-1,np.subtract(mask2,1)))))
+    plt.show()
+    
+    #mask2[int(self._com[0])-10:int(self._com[0])+20, int(self._com[1])-10: int(self._com[1])+20] = 1
+    #data_temp = np.multiply(self._twoddata, np.multiply(-1,np.subtract(mask2,1)))
+    #mask[300,409] = 1 #mask[398,388] = 1 # Works only for my PC at home
+    mask[666,388] = 1 
+    mask[764,409] = 1 
+    mask[1006,274] = 1 
+    #mask[:int(self._com[0])]=1 # Works only for my PC at home
+    mask[int(self._com[0])+1:]=1
     #np.save('mask.npy', mask)
     if self._VERBOSE:
       plt.imshow(np.log10(np.multiply(self._twoddata,np.multiply(-1,np.subtract(mask,1)))))
