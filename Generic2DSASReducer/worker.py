430df623c9bc194d56a571f62808451327a258e7
Klaus Götz <kgoetz91@t-online.de>
2020-08-14 23:17:29 +0200

    started work on worker class

---
+++ Generic2DSASReducer/worker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.8
+"""This is the worker module that handles the top user interface."""
+
+import numpy as np
+
+from loader.saxsans import SaxansLoader
+from twodoperations.twodoperations import DataSet
+
+class Worker:
+  
+  def _setup_worker(self, parameter_file):
+  
+  def __init__(self, parameter_file):
+    self._setup_worker(parameter_file)
